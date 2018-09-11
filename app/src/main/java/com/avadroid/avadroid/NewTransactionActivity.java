a46c32d2043ac3b76d15428468623ba3ec56161e
brandon-fenty <brandonfenty@gmail.com>
2018-09-11 10:36:22 -0700

    Started search functionality. Come back with API.

--- app/src/main/java/com/avadroid/avadroid/NewTransactionActivity.java
+++ app/src/main/java/com/avadroid/avadroid/NewTransactionActivity.java
@@ -10,27 +10,11 @@ import java.util.List;
 
 public class NewTransactionActivity extends AppCompatActivity {
 
-    private Spinner spinner1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_transaction);
 
-        addItemsOnSpinner1();
-//        addListenerOnButton();
-//        addListenerOnSpinnerItemSelection();
     }
 
-    public void addItemsOnSpinner1() {
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
-        List<String> list = new ArrayList<>();
-        list.add("Item 1");
-        list.add("Item 2");
-        list.add("Item 3");
-        list.add("Item 4");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(dataAdapter);
-    }
 }
