51ecb98089b87c2db89be1d89ebe37f7ddd9b6d3
GoldBeardSea <timbusch1@gmail.com>
2018-09-14 10:55:34 -0700

    working on auth

--- app/src/main/java/com/avadroid/avadroid/MainActivity.java
+++ app/src/main/java/com/avadroid/avadroid/MainActivity.java
@@ -4,6 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.android.volley.Cache;
@@ -16,11 +26,17 @@ import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    public static boolean loggedIn = false;
+
 
 
     @Override
@@ -29,15 +45,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        if(!loggedIn){
+            makeToast("Please login for \ntax code checks");
+        }
+
+        final Button login = findViewById(R.id.login_button);
+        final EditText mUsername = findViewById(R.id.username);
+        final EditText mPassword = findViewById(R.id.password);
+
+        mPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId== EditorInfo.IME_ACTION_DONE){
+                    login(mUsername.getText().toString(), mPassword.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
     }
-
-    @OnClick(R.id.login_button)
-    protected void loginButtonHandler () {
-        Intent intent = new Intent(MainActivity.this, TaxInformationActivity.class);
-
-        startActivity(intent);
-    }
-
+    
     @Override
     protected void onStart() {
         super.onStart();
@@ -66,4 +96,77 @@ public class MainActivity extends AppCompatActivity {
             });
         mRequestQueue.add(stringRequest);
     }
+
+
+    public void login(String username, String password){
+        if (username == null || password == null) {
+
+            makeToast("Enter Username and Password");
+
+        } else {
+            RequestQueue queue = Volley.newRequestQueue(this);
+
+            try {
+                username = URLEncoder.encode(username, "UTF-8");
+                password = URLEncoder.encode(password, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            String url = "https://avatax-server.herokuapp.com/authorization?username="
+                    + username + "&password=" + password;
+            Log.d("URL PASS", url);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("RESPONSE: ", response);
+                            boolean authStatus = Boolean.parseBoolean(response.toString());
+
+                            if(authStatus){
+
+                                loggedIn = authStatus;
+
+                                makeToast("You are now logged in \nGoing to search now");
+                                Log.d("AUTH STATUS", String.valueOf(loggedIn));
+
+                                goToTaxInfoActivity();
+
+                            } else {
+
+                                loggedIn = false;
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            queue.add(stringRequest);
+        }
+    }
+
+    private void goToTaxInfoActivity() {
+        Intent intent = new Intent(this, TaxInformationActivity.class);
+        startActivity(intent);
+    }
+
+    private void makeToast(String s) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast,
+                (ViewGroup) findViewById(R.id.toast_container));
+
+        TextView text = layout.findViewById(R.id.text);
+        text.setText(s);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, -400);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+
+
 }
